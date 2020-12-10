639ea88104ef9651f796515dc9e54dbf4c7f684c
DuyAnh1202 <duyanh1202tm@gmail.com>
2020-12-10 20:15:01 +0700

    revert1

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 console.log("Hello world")
 console.log("hahaha");
+<<<<<<< HEAD
 console.log("bao lauu");
 console.log("binh")
+=======
+console.log("bao lauu");
+>>>>>>> parent of 665ebb9... fifth commit
